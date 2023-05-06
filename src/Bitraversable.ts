1f87907a5a09f586872ef2ef8f98d211662e0d92
Jacob Alford <jacob.alford@me.com>
2023-05-05 18:00:57 -0600

    feat(traverseLR): add traverseLeft and traverseRight to Bitraversal

--- src/Bitraversable.ts
+++ src/Bitraversable.ts
@@ -201,3 +201,125 @@ export type PipeableBitraverse<T extends URIS2> = {
     g: (b: B) => HKT<F, D>,
   ) => (fa: Kind2<T, A, B>) => HKT<F, Kind2<T, C, D>>
 }
+
+// -------------------------------------------------------------------------------------
+// Utilities
+// -------------------------------------------------------------------------------------
+
+/**
+ * @since 1.1.0
+ * @category Utilities
+ */
+export function traverseLeft<T extends URIS2>(
+  T: Bitraversable2<T>,
+): {
+  <F extends URIS4>(F: Applicative4<F>): <S, R, E, A, B>(
+    f: (a: A) => Kind4<F, S, R, E, B>,
+  ) => <C>(ta: Kind2<T, A, C>) => Kind4<F, S, R, E, Kind2<T, B, C>>
+  <F extends URIS3>(F: Applicative3<F>): <R, E, A, B>(
+    f: (a: A) => Kind3<F, R, E, B>,
+  ) => <C>(ta: Kind2<T, A, C>) => Kind3<F, R, E, Kind2<T, B, C>>
+  <F extends URIS3, E>(F: Applicative3C<F, E>): <R, A, B>(
+    f: (a: A) => Kind3<F, R, E, B>,
+  ) => <C>(ta: Kind2<T, A, C>) => Kind3<F, R, E, Kind2<T, B, C>>
+  <F extends URIS2>(F: Applicative2<F>): <E, A, B>(
+    f: (a: A) => Kind2<F, E, B>,
+  ) => <C>(ta: Kind2<T, A, C>) => Kind2<F, E, Kind2<T, B, C>>
+  <F extends URIS2, E>(F: Applicative2C<F, E>): <A, B>(
+    f: (a: A) => Kind2<F, E, B>,
+  ) => <C>(ta: Kind2<T, A, C>) => Kind2<F, E, Kind2<T, B, C>>
+  <F extends URIS>(F: Applicative1<F>): <A, B>(
+    f: (a: A) => Kind<F, B>,
+  ) => <C>(ta: Kind2<T, A, C>) => Kind<F, Kind2<T, B, C>>
+  <F>(F: Applicative<F>): <A, B>(
+    f: (a: A) => HKT<F, B>,
+  ) => <C>(ta: Kind2<T, A, C>) => HKT<F, Kind2<T, B, C>>
+}
+export function traverseLeft<T>(
+  T: Bitraversable<T>,
+): {
+  <F extends URIS4>(F: Applicative4<F>): <S, R, E, A, B>(
+    f: (a: A) => Kind4<F, S, R, E, B>,
+  ) => <C>(ta: HKT2<T, A, C>) => Kind4<F, S, R, E, HKT2<T, B, C>>
+  <F extends URIS3>(F: Applicative3<F>): <R, E, A, B>(
+    f: (a: A) => Kind3<F, R, E, B>,
+  ) => <C>(ta: HKT2<T, A, C>) => Kind3<F, R, E, HKT2<T, B, C>>
+  <F extends URIS3, E>(F: Applicative3C<F, E>): <R, A, B>(
+    f: (a: A) => Kind3<F, R, E, B>,
+  ) => <C>(ta: HKT2<T, A, C>) => Kind3<F, R, E, HKT2<T, B, C>>
+  <F extends URIS2>(F: Applicative2<F>): <E, A, B>(
+    f: (a: A) => Kind2<F, E, B>,
+  ) => <C>(ta: HKT2<T, A, C>) => Kind2<F, E, HKT2<T, B, C>>
+  <F extends URIS2, E>(F: Applicative2C<F, E>): <A, B>(
+    f: (a: A) => Kind2<F, E, B>,
+  ) => <C>(ta: HKT2<T, A, C>) => Kind2<F, E, HKT2<T, B, C>>
+  <F extends URIS>(F: Applicative1<F>): <A, B>(
+    f: (a: A) => Kind<F, B>,
+  ) => <C>(ta: HKT2<T, A, C>) => Kind<F, HKT2<T, B, C>>
+  <F>(F: Applicative<F>): <A, B>(
+    f: (a: A) => HKT<F, B>,
+  ) => <C>(ta: HKT2<T, A, C>) => HKT<F, HKT2<T, B, C>>
+} {
+  return <F>(F: Applicative<F>) => <A, B>(f: (a: A) => HKT<F, B>) => <C>(
+    ta: HKT2<T, A, C>,
+  ): HKT<F, HKT2<T, B, C>> => T.bitraverse(F)(ta, f, F.of)
+}
+
+/**
+ * @since 1.1.0
+ * @category Utilities
+ */
+export function traverseRight<T extends URIS2>(
+  T: Bitraversable2<T>,
+): {
+  <F extends URIS4>(F: Applicative4<F>): <S, R, E, B, C>(
+    g: (b: B) => Kind4<F, S, R, E, C>,
+  ) => <A>(ta: Kind2<T, A, B>) => Kind4<F, S, R, E, Kind2<T, A, C>>
+  <F extends URIS3>(F: Applicative3<F>): <R, E, B, C>(
+    g: (b: B) => Kind3<F, R, E, C>,
+  ) => <A>(ta: Kind2<T, A, B>) => Kind3<F, R, E, Kind2<T, A, C>>
+  <F extends URIS3, E>(F: Applicative3C<F, E>): <R, B, C>(
+    g: (b: B) => Kind3<F, R, E, C>,
+  ) => <A>(ta: Kind2<T, A, B>) => Kind3<F, R, E, Kind2<T, A, C>>
+  <F extends URIS2>(F: Applicative2<F>): <E, B, C>(
+    g: (b: B) => Kind2<F, E, C>,
+  ) => <A>(ta: Kind2<T, A, B>) => Kind2<F, E, Kind2<T, A, C>>
+  <F extends URIS2, E>(F: Applicative2C<F, E>): <B, C>(
+    g: (b: B) => Kind2<F, E, C>,
+  ) => <A>(ta: Kind2<T, A, B>) => Kind2<F, E, Kind2<T, A, C>>
+  <F extends URIS>(F: Applicative1<F>): <B, C>(
+    g: (b: B) => Kind<F, C>,
+  ) => <A>(ta: Kind2<T, A, B>) => Kind<F, Kind2<T, A, C>>
+  <F>(F: Applicative<F>): <B, C>(
+    g: (b: B) => HKT<F, C>,
+  ) => <A>(ta: Kind2<T, A, B>) => HKT<F, Kind2<T, A, C>>
+}
+export function traverseRight<T>(
+  T: Bitraversable<T>,
+): {
+  <F extends URIS4>(F: Applicative4<F>): <S, R, E, B, C>(
+    g: (b: B) => Kind4<F, S, R, E, C>,
+  ) => <A>(ta: HKT2<T, A, B>) => Kind4<F, S, R, E, HKT2<T, A, C>>
+  <F extends URIS3>(F: Applicative3<F>): <R, E, B, C>(
+    g: (b: B) => Kind3<F, R, E, C>,
+  ) => <A>(ta: HKT2<T, A, B>) => Kind3<F, R, E, HKT2<T, A, C>>
+  <F extends URIS3, E>(F: Applicative3C<F, E>): <R, B, C>(
+    g: (b: B) => Kind3<F, R, E, C>,
+  ) => <A>(ta: HKT2<T, A, B>) => Kind3<F, R, E, HKT2<T, A, C>>
+  <F extends URIS2>(F: Applicative2<F>): <E, B, C>(
+    g: (b: B) => Kind2<F, E, C>,
+  ) => <A>(ta: HKT2<T, A, B>) => Kind2<F, E, HKT2<T, A, C>>
+  <F extends URIS2, E>(F: Applicative2C<F, E>): <B, C>(
+    g: (b: B) => Kind2<F, E, C>,
+  ) => <A>(ta: HKT2<T, A, B>) => Kind2<F, E, HKT2<T, A, C>>
+  <F extends URIS>(F: Applicative1<F>): <B, C>(
+    g: (b: B) => Kind<F, C>,
+  ) => <A>(ta: HKT2<T, A, B>) => Kind<F, HKT2<T, A, C>>
+  <F>(F: Applicative<F>): <B, C>(
+    g: (b: B) => HKT<F, C>,
+  ) => <A>(ta: HKT2<T, A, B>) => HKT<F, HKT2<T, A, C>>
+} {
+  return <F>(F: Applicative<F>) => <B, C>(g: (b: B) => HKT<F, C>) => <A>(
+    ta: HKT2<T, A, B>,
+  ): HKT<F, HKT2<T, A, C>> => T.bitraverse(F)(ta, F.of, g)
+}
